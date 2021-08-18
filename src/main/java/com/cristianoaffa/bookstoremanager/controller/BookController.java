7d1d4a87005e8bf744072452bfded8eae5504d5b
cristianoaffa <cristianoaffa@gmail.com>
2021-08-17 22:24:33 -0300

    adding system.properties

--- src/main/java/com/cristianoaffa/bookstoremanager/controller/BookController.java
+++ src/main/java/com/cristianoaffa/bookstoremanager/controller/BookController.java
@@ -4,13 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
 	
+	/*	
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Success method return")
+	})
+	*/	
+	@ApiOperation(value = "Return an example hello world")
 	@GetMapping
-	public String hello() {
-		return "hello bookstore manager!";		
+	public String hello() {		
+		return "hello bookstore manager!";	
+		
 	}
 
 }
